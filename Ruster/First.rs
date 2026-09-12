af8833708db1ce20b9aa96cea73c44b880c73033
Andersonfritzen <Cristianefernandafritzen@gmail.com>
2026-09-12 01:15:58 -0400

    First.rs-upd5

--- Ruster/First.rs
+++ Ruster/First.rs
@@ -1,25 +1,39 @@
 fn main() {
-    println!("I'm not gonna sugarcoat it!");
- 
-    let algo_nah: &str = "String";
-    println!("{}", algo_nah);
-    let one: &str = "Anderson";
-    let two: &str = "14";
-    let three: &str = "1.72";
-    println!("{one}, {two}, {three}");
-    let a: i32 = 10;
-    let b: i32 = 3;
-    println!("{}", a + b);
-    println!("{}", a - b);
-    println!("{}", a * b);
-    println!("{}", a / b);
-    println!("{}", a % b);
-    let c: i16 = 5;
-    if c % 2 != 0 {
-        println!("Impar, eu acho");
+   fn lambda(um: i128, dois: i128) -> i128 {
+        um + dois
+   }
+   println!("{}", lambda(10, 999999999999999999999999999));
+   fn verify_i(n1: i32, n2: i32) -> i32 {
+        if n1 < n2 {n2} else {n1}
+   }
+   println!("{}", verify_i(55, 50));
+    let mut num: i8 = 10;
+    while num > 0 {
+        println!("{num}");
+        num -= 1 
     }
-    for n in 1..=10 {
-        println!("{}", n);
-    }
-    
+    println!("Num sei");
+    let dia: i8= 7;
+match dia {
+    1 => println!("Segunda"),
+    2 => println!("Terça"),
+    3 => println!("Quarta"),
+    4 => println!("Quinta"),
+    5 => println!("Sexta"),
+    6 => println!("Sábado"),
+    7 => println!("Domingo"),
+    _ => println!("Dia inválido"),
 }
+for n in 1..=20 {
+    if n % 15 == 0 {
+        println!("FizzBuzz");
+    } else if n % 3 == 0 {
+        println!("Fizz");
+    } else if n % 5 == 0 {
+        println!("Buzz");
+    } else {
+        println!("{n}");
+}
+}
+}
+// Cortei parte do código pra ficar menor e reduzir o tempo de compilação
